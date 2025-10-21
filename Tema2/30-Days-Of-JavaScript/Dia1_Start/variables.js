675877630608644f2ede299b19340393cad97453
martinalejandroo25 <martinalejandroo25@gmail.com>
2025-10-21 18:09:11 +0200

    30 dias de python dia 1 y cambio de carpetas

---
+++ Tema2/30-Days-Of-JavaScript/Dia1_Start/variables.js
@@ -0,0 +1,17 @@
+// declarar diferentes variables de diferentes tipos de datos
+
+let firstName = 'martin'
+let lastName = 'Garbiso'
+let country = 'Almeria'
+let city = 'Turre'
+let age = 100
+let isMarried = false
+
+// declarar varibles con valores de numeros
+
+const gravity = 9.81
+const bolingPoint = 100
+const PI = 3.14
+
+//variables puedes declararse en una sola linea separados por comas
+let name = 'martin', job = 'hacker', live = 'turre'
